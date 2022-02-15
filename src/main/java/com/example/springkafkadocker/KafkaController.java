46b8cf1cbfa53acd97c8d7b0e912de7617db90b8
btdevika <devika.bt@knoldus.com>
2022-02-15 18:07:53 +0530

    kafka with springboot and docker

---
+++ src/main/java/com/example/springkafkadocker/KafkaController.java
@@ -0,0 +1,19 @@
+package com.example.springkafkadocker;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+    private KafkaProducer producer;
+
+    public KafkaController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping("/publish")
+    public void writeMessageToTopic(@RequestParam("message") String message){
+        this.producer.writeMessage(message);
+    }
+}
